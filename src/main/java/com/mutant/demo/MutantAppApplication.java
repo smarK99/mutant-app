275535f8dcd60cf8d9e69a57455b6bf63a707d38
smarK99 <santikb99@gmail.com>
2024-10-14 08:12:26 -0300

    todo ok para empezar

---
+++ src/main/java/com/mutant/demo/MutantAppApplication.java
@@ -0,0 +1,13 @@
+package com.mutant.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mutant/demo/MutantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutant.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
